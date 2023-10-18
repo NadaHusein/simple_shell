5469e4da31d1bed26ce9433122b4a797b4421653
NadaHusein <nada.hussein796@gmail.com>
2023-10-18 16:46:33 -0700

    first commit

---
+++ main.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+/**
+ * main - simple shell main function
+ * @ac: count of arguments
+ * @av: Arguments
+ *
+ * Return: 0 Always (success)
+ */
+int main(int ac, char **argv)
+{
+	char *line = NULL;
+	char **command = NULL;
+	int status = 0;
+	(void) ac;
+	(void) argv;
+
+	while (1)
+	{
+		line = read_line();
+		if (line == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			return (status);
+		}
+
+
+		
+		command = tokenizer(line);
+		if (!command)
+			continue;
+
+		status = _excute(command, argv);
+	}
+}
